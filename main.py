0d6a6a0605b6e5f23df4a49c62d6d18f4169970f
Gayathri788-ai <gayathridevipullagura@gmail.com>
2026-05-31 12:25:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from datetime import datetime
 
+name = input("Enter your name: ")
+print("Welcome,", name)
+
+print("1. Study Tips")
+print("2. Motivation Quote")
+print("3. Current Date and Time")
+
+choice = input("Enter choice: ")
+
+if choice == "1":
+    print("Study Tip: Practice coding daily.")
+elif choice == "2":
+    print("Success comes from consistent effort.")
+elif choice == "3":
+    print(datetime.now())
+else:
+    print("Invalid choice")
